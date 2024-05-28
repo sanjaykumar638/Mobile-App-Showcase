f9afc9e5c8b2ab91ca216e913c06dab4a41591ac
sanjaykumar638 <sk7067194@gmail.com>
2024-05-28 12:05:26 +0530

    solution

---
+++ src/components/Desktop2.js
@@ -0,0 +1,367 @@
+import React from "react";
+import "./Desktop2.css";
+
+export const Desktop2 = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap">
+          <img className="pink-header-bg" alt="Pink header bg" src="./assests/pink-header-bg 1.png" />
+          <div className="group">
+            <div className="overlap-group">
+              <div className="overlap-wrapper">
+                <div className="overlap-2">
+                  <img className="img" alt="Group" src="./assests/Group 1.png" />
+                  <img className="vector" alt="Vector" src="./assests/Vector 1.png" />
+                  <img className="hero-mock" alt="Hero mock" src="./assests/hero_mock 1.png" />
+                  <img className="user" alt="User" src="./assests/user 1.png" />
+                  <img className="group-2" alt="Group" src="./assests/Group-29 1.png" />
+                  <img className="rt" alt="Rt" src="./assests/rt 1.png" />
+                  <div className="background-shape">
+                    <div className="overlap-group-2">
+                      <img className="vector-2" alt="Vector" src="./assests/Vector1.png" />
+                      <img className="vector-3" alt="Vector" src="./assests/Vector1.png" />
+                    </div>
+                  </div>
+                  <img className="vector-4" alt="Vector" src="./assests/Vector.png" />
+                  <img className="vector-5" alt="Vector" src="./assests/Vector.png" />
+                  <img className="shape" alt="Shape" src="./assests/shape_11 1.png" />
+                </div>
+              </div>
+              <div className="group-3">
+                <div className="best-mobile-app">
+                  Best Mobile <br />
+                  App Showcase
+                </div>
+                <p className="aenean-dictum-odio">
+                  Aenean dictum odio dapibus turpis dapibus vestibulum. Mauris quis massa <br />
+                  nisi. Nullam porta lorem at volutpat euismod. Proin in ex nunc.
+                </p>
+                <div className="div-wrapper">
+                  <div className="text-wrapper">Read More</div>
+                </div>
+              </div>
+              <div className="home-about-us">
+                Home&nbsp;&nbsp;&nbsp;&nbsp;About
+                Us&nbsp;&nbsp;&nbsp;&nbsp;Features&nbsp;&nbsp;&nbsp;&nbsp;
+                Contact
+              </div>
+            </div>
+          </div>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-3">
+              <div className="group-7">
+                <div className="group-8">
+                  <div className="overlap-group-3">
+                    <img className="vector-9" alt="Vector" src="./assests/Vector 7.png" />
+                    <img className="vector-10" alt="Vector" src="./assests/Vector 6.png" />
+                    <div className="ellipse" />
+                    <img className="content" alt="Content" src="./assests/content 1.png" />
+                    <div className="perfect-UI-design">Perfect Ui Design</div>
+                    <p className="praesent-ac-vehicula">
+                      Praesent Ac Vehicula Sapien. Sed Sollicitudin Molestie Consequat. Ut Vitae Ante Ut Mi Vehicula
+                      Vulputate.
+                    </p>
+                    <div className="ellipse-2" />
+                    <img className="vector-12" alt="Vector" src="./assests/Arrow.png" />
+                  </div>
+                </div>
+                <div className="group-10">
+                  <div className="overlap-4">
+                    <img className="vector-9" alt="Vector" src="./assests/Vector 7.png" />
+                    <img className="vector-13" alt="Vector" src="./assests/Vector 6.png" />
+                    <img className="ellipse-3" alt="Ellipse" src="./assests/Ellipse 1.png" />
+                    <img className="content-2" alt="Content" src="./assests/content 1.png" />
+                    <div className="perfect-UI-design-2">Perfect Ui Design</div>
+                    <p className="p">
+                      Praesent Ac Vehicula Sapien. Sed Sollicitudin Molestie Consequat. Ut Vitae Ante Ut Mi Vehicula
+                      Vulputate.
+                    </p>
+                    <div className="ellipse-4" />
+                    <img className="vector-15" alt="Vector" src="./assests/Arrow.png" />
+                  </div>
+                </div>
+                <img className="girl" alt="Girl" src="./assests/girl 1.png" />
+                <div className="group-12">
+                  <div className="overlap-4">
+                    <img className="vector-9" alt="Vector" src="./assests/Vector 7.png" />
+                    <img className="vector-13" alt="Vector" src="./assests/Vector 6.png" />
+                    <img className="ellipse-3" alt="Ellipse" src="./assests/Ellipse 1.png" />
+                    <img className="content-2" alt="Content" src="./assests/content 1.png" />
+                    <div className="perfect-UI-design-2">Perfect Ui Design</div>
+                    <p className="p">
+                      Praesent Ac Vehicula Sapien. Sed Sollicitudin Molestie Consequat. Ut Vitae Ante Ut Mi Vehicula
+                      Vulputate.
+                    </p>
+                    <div className="ellipse-4" />
+                    <img className="vector-15" alt="Vector" src="./assests/Arrow.png" />
+                  </div>
+                </div>
+              </div>
+              <p className="engaging-spacious">
+                Engaging &amp; Spacious <br />
+                school Campus
+              </p>
+              <p className="hise-sed-augue-vitae">
+                Hise Sed Augue Vitae Felis Pellentesque Varius Nec Quis Nunc. Morbi Mauris Augue, Pulvinar Quis Luctus
+                Eget. Phasellus Gravida Lacus Quis Eros Lobortis, Nec Dapibus Quam Gravida.
+              </p>
+            </div>
+          </div>
+          <div className="group-13">
+            <div className="overlap-5">
+              <img className="zas" alt="Zas" src="./assests/zas 1.png" />
+              <img className="element-removebg" alt="Element removebg" src="./assests/4667617-removebg-preview 1.png" />
+              <img className="xc" alt="Xc" src="./assests/xc-2 1.png" />
+            </div>
+            <p className="easy-and-perfect">
+              Easy And Perfect Solution For <br />
+              your Business App
+            </p>
+            <div className="overlap-6">
+              <p className="aliquam-aliquet">
+                Aliquam Aliquet Purus A Est Consequat Lobortis. Etiam Vehicula Suscipit Purus, Eget Ullamcorper Augue
+                Blandit Vitae. Ut Eu Euismod Felis. Etiam At Varius Quam. Vivamus Lacinia Pulvinar Turpis In Suscipit.
+                Aenean Mattis Enim Ut Pretium Gravida. Sed Fermentum A Lacus Bibendum Convallis.
+                <br />
+                <br />
+                consequat Purus Aliquet A Est Aliquam Lobortis. Etiam Vehicula Suscipit Purus, Eget Ullamcorper Augue
+                Blandit Vitae....
+              </p>
+              <div className="rectangle" />
+              <div className="text-wrapper-2">Read More</div>
+            </div>
+          </div>
+          <div className="group-14">
+            <div className="overlap-7">
+              <div className="group-15">
+                <div className="overlap-group-4">
+                  <div className="rectangle-2" />
+                  <div className="ellipse-5" />
+                  <div className="ellipse-6" />
+                  <div className="ellipse-7" />
+                  <img className="vector-17" alt="Vector" src="./assests/Profile.png" />
+                  <div className="text-wrapper-3">Make A Profile</div>
+                  <p className="text-wrapper-4">
+                    Aliquam varius ligula nec leo tempus porta. Vestibulum suscipit leo at nunc imperdiet, quis lacinia
+                    nisi euismod.
+                  </p>
+                </div>
+              </div>
+              <div className="group-16">
+                <div className="overlap-group-4">
+                  <div className="rectangle-2" />
+                  <div className="ellipse-8" />
+                  <div className="ellipse-9" />
+                  <div className="ellipse-10" />
+                  <img className="vector-17" alt="Vector" src="./assests/Profile.png" />
+                  <div className="text-wrapper-3">Make A Profile</div>
+                  <p className="text-wrapper-4">
+                    Aliquam varius ligula nec leo tempus porta. Vestibulum suscipit leo at nunc imperdiet, quis lacinia
+                    nisi euismod.
+                  </p>
+                </div>
+              </div>
+              <div className="group-17">
+                <div className="overlap-8">
+                  <div className="rectangle-2" />
+                  <div className="group-18">
+                    <div className="overlap-group-5">
+                      <div className="ellipse-11" />
+                      <div className="ellipse-12" />
+                      <div className="ellipse-13" />
+                      <img className="vector-18" alt="Vector" src="./assests/Profile.png" />
+                    </div>
+                  </div>
+                  <div className="text-wrapper-5">Make A Profile</div>
+                  <p className="text-wrapper-6">
+                    Aliquam varius ligula nec leo tempus porta. Vestibulum suscipit leo at nunc imperdiet, quis lacinia
+                    nisi euismod.
+                  </p>
+                </div>
+              </div>
+              <img className="dsd-3" alt="Dsd" src="dsd-3.png" />
+              <p className="text-wrapper-7">How does This App Work?</p>
+              <img className="element-removebg-2" alt="Element removebg" src="./assests/4934434-removebg-preview 1.png" />
+            </div>
+          </div>
+          <p className="designed-worked-by">
+            Designed &amp; Worked By The Latest <br />
+            Partners
+          </p>
+          <img className="sa" alt="Sa" src="./assests/sa 1.png" />
+          <img className="sa-2" alt="Sa" src="./assests/sa 1.png" />
+          <img
+            className="freelance-man-is"
+            alt="Freelance man is"
+            src="./assests/freelance-man-is-working-removebg-preview 1.png"
+          />
+          <div className="rectangle-3" />
+          <div className="rectangle-4" />
+          <p className="text-wrapper-8">Aliquam varius ligula nec leo tempus porta.</p>
+          <div className="rectangle-5" />
+          <p className="text-wrapper-9">Suspendisse vitae varius diam, a vulputate urna.</p>
+          <div className="rectangle-6" />
+          <p className="text-wrapper-10">Aliquam aliquet purus eget lacus pretium.</p>
+          <p className="lorem-ipsum-is">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting,
+          </p>
+          <div className="group-19">
+            <div className="overlap-9">
+              <img className="screenshot" alt="Screenshot" src="./assests/screenshot13 1.png" />
+              <div className="overlap-10">
+                <img className="screenshot-2" alt="Screenshot" src="./assests/screenshot17 1.png" />
+                <img className="screenshot-3" alt="Screenshot" src="./assests/screenshot14 1.png" />
+                <div className="overlap-group-6">
+                  <img className="screenshot-4" alt="Screenshot" src="./assests/screenshot15 1.png" />
+                  <img className="phone" alt="Phone" src="./assests/phone-mokeup2 1.png" />
+                  <p className="saepe-quo-labore">
+                    Saepe Quo Labore Aenean Dictumst Expedita Commodi Auctor, Nisl, Lorem Iusto Feugiat Nemo Reiciendis
+                    Laboris.
+                  </p>
+                </div>
+              </div>
+              <img className="screenshot-5" alt="Screenshot" src="./assests/screenshot16 1.png" />
+              <div className="text-wrapper-11">App Screenshots</div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-11">
+          <div className="rectangle-7" />
+          <img className="vector-22" alt="Vector" src="./assests/smaller than.png" />
+          <img className="vector-23" alt="Vector" src="./assests/greater that.png" />
+          <div className="review-s-from">Review's From Students</div>
+          <div className="group-20">
+            <div className="overlap-12">
+              <div className="text-wrapper-12">Jenny Wilson</div>
+              <img className="vector-24" alt="Vector" src="./assests/empty star.png" />
+              <img className="vector-25" alt="Vector" src="./assests/empty star.png" />
+              <img className="vector-26" alt="Vector" src="./assests/star.png" />
+              <img className="vector-27" alt="Vector" src="./assests/star.png" />
+              <img className="vector-28" alt="Vector" src="./assests/star.png" />
+              <div className="overlap-13">
+                <div className="overlap-group-7">
+                  <img className="aro" alt="Aro" src="./assests/aro 1.png" />
+                  <p className="per-sed-mattis">
+                    Per Sed, Mattis. Integer Viverra Euismod Maecenas Incidunt, Phasellus Consequatur Aliquam Nihil
+                    Temporibus In Assumenda? Aute Praesentium Fugiat. Perspiciatis, Ultrices Deserunt Convallis, Eius At
+                    Non.
+                  </p>
+                </div>
+                <div className="overlap-group-8">
+                  <img className="vector-29" alt="Vector" src="./assests/Vector 14.png" />
+                  <div className="ellipse-14" />
+                  <img className="mask-group" alt="Mask group" src="./assests/Mask group.png" />
+                </div>
+                <img className="inverted-commas-top" alt="Inverted commas top" src="./assests/inverted-commas-top 1.png" />
+              </div>
+            </div>
+          </div>
+          <div className="group-21">
+            <div className="overlap-12">
+              <div className="text-wrapper-12">Jenny Wilson</div>
+              <img className="vector-24" alt="Vector" src="./assests/empty star.png" />
+              <img className="vector-25" alt="Vector" src="./assests/star.png" />
+              <img className="vector-26" alt="Vector" src="./assests/star.png" />
+              <img className="vector-27" alt="Vector" src="./assests/star.png" />
+              <img className="vector-28" alt="Vector" src="./assests/star.png" />
+              <div className="overlap-13">
+                <div className="overlap-group-7">
+                  <img className="aro" alt="Aro" src="./assests/aro 1.png" />
+                  <p className="per-sed-mattis">
+                    Per Sed, Mattis. Integer Viverra Euismod Maecenas Incidunt, Phasellus Consequatur Aliquam Nihil
+                    Temporibus In Assumenda? Aute Praesentium Fugiat. Perspiciatis, Ultrices Deserunt Convallis, Eius At
+                    Non.
+                  </p>
+                </div>
+                <div className="overlap-group-8">
+                  <img className="vector-29" alt="Vector" src="./assests/Vector 14.png" />
+                  <div className="ellipse-14" />
+                  <img className="mask-group" alt="Mask group" src="./assests/Mask group.png" />
+                </div>
+                <img className="inverted-commas-top" alt="Inverted commas top" src="./assests/inverted-commas-top 1.png" />
+              </div>
+            </div>
+          </div>
+          <div className="rectangle-8" />
+          <p className="quick-links-useful">
+            Quick
+            Links&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            Useful
+            Links&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            School Hours
+          </p>
+          <div className="rectangle-9" />
+          <div className="text-wrapper-13">Subscribe Our Newsletter !</div>
+          <p className="elementum-ex">
+            Elementum Ex Similique Sollicitudin Eveniet Sem Eveniet Proin, Iste Euismod! Quia! Fugiat Molestie Leo
+            Placerat, Tenetur.
+          </p>
+          <img className="vector-31" alt="Vector" src="vector-15-2.svg" />
+          <div className="rectangle-10" />
+          <div className="rectangle-11" />
+          <div className="SUBSRIBE">Subsribe</div>
+          <div className="group-25">
+            <p className="text-wrapper-14">
+              Home <br />
+              about Us
+              <br />
+              careers
+              <br />
+              news &amp; Articles
+            </p>
+            <img className="vector-32" alt="Vector" src="./assests/Vector 17.png" />
+            <img className="vector-33" alt="Vector" src="./assests/Vector 17.png" />
+            <img className="vector-34" alt="Vector" src="./assests/Vector 17.png" />
+            <img className="vector-35" alt="Vector" src="./assests/Vector 17.png" />
+          </div>
+          <div className="group-26">
+            <div className="text-wrapper-14">
+              Help Center
+              <br />
+              contact Us
+              <br />
+              faq
+              <br />
+              parent Community
+            </div>
+            <img className="vector-32" alt="Vector" src="./assests/Vector 17.png" />
+            <img className="vector-33" alt="Vector" src="./assests/Vector 17.png" />
+            <img className="vector-34" alt="Vector" src="./assests/Vector 17.png" />
+            <img className="vector-35" alt="Vector" src="./assests/Vector 17.png" />
+          </div>
+          <p className="element-AM-PM-monday">8 Am - 5 Pm , Monday - Saturday</p>
+          <p className="aut-quae-convallis">
+            Aut, Quae Convallis Minim Cum Ornare! Pede Ut Rem, Totam Dictum Convallis.
+          </p>
+          <img className="vector-36" alt="Vector" src="./assests/watch.png" />
+          <img className="group-27" alt="Group" src="./assests/location.png" />
+          <div className="vector-wrapper">
+            <img className="vector-37" alt="Vector" src="./assests/FB.png" />
+          </div>
+          <div className="img-wrapper">
+            <img className="vector-38" alt="Vector" src="./assests/insta.png" />
+          </div>
+          <div className="group-wrapper">
+            <img className="group-28" alt="Group" src="./assests/Twr.png" />
+          </div>
+          <p className="vero-tempor-morbi">
+            Vero, Tempor Morbi, Adipiscing Aliqua Nonummy Proident Perferendis? Laboris Lacus Quidem Repellendus Quasi.
+          </p>
+          <div className="rectangle-12" />
+          <p className="PRIVACY-POLICY">
+            PRIVACY POLICY&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; SUPPORT&nbsp;&nbsp;&nbsp;&nbsp;
+            |&nbsp;&nbsp;&nbsp;&nbsp;TERMS &amp; CONDITION
+          </p>
+          <p className="text-wrapper-15">Copyright © 2022 Educator. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default Desktop2;
